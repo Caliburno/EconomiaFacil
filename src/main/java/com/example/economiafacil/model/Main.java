87517a2c23a665d3ebec29f15daa44ddf4d70e2c
Dario <dario.buffa@gmail.com>
2025-02-20 17:49:05 -0300

    Edit main

--- src/main/java/com/example/economiafacil/model/Main.java
+++ src/main/java/com/example/economiafacil/model/Main.java
@@ -8,12 +8,7 @@ public class Main {
 
         Scanner entrada = new Scanner(System.in);
         Registro registro = new Registro();
-        Movimientos movimiento = new Movimientos(entrada.nextLine(),
-                entrada.nextDouble(),
-                new LocalDateTime());
 
-        System.out.println("Ingrese un movimiento indicando concepto y monto"); //esto después es front, el print está para ir viendo
 
-        registro.nuevoMovimiento();
     }//fin main
 }
